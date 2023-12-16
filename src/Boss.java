32bfa184198d81e8321c9a48081a8ba8cc1ce388
Amin_stors <uli2930tong@gmail.com>
2023-12-16 19:54:48 +0300

    home work 5

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Boss {
+public class Boss {
+    private int health;
+    private int damage;
+    private String typeOfProtection;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getTypeOfProtection() {
+        return typeOfProtection;
+    }
+
+    public void setTypeOfProtection(String typeOfProtection) {
+        this.typeOfProtection = typeOfProtection;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Boss boss = new Boss();
+        boss.setHealth(250);
+        boss.setDamage(50);
+        boss.setTypeOfProtection("Щит");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Health: " + boss.getHealth() +
+                " Damage:" + boss.getDamage() +
+                " Type of protection: " + boss.getTypeOfProtection());
+
+        Hero[] heroes = createHeroes();
+        for (Hero hero : heroes) {
+            System.out.println("Health: " + hero.getHealth() +
+                    " Damage:" + hero.getDamage() +
+                    " Super power: " + hero.getSuperPower());
         }
+    }
+
+    public static Hero[] createHeroes() {
+        Hero hero1 = new Hero(150, 25, "Мощный удар");
+        Hero hero2 = new Hero(135, 10);
+        Hero hero3 = new Hero(127, 5, "Болтун");
+
+        Hero[] heroes = {hero1, hero2, hero3};
+        return heroes;
     }
 }
